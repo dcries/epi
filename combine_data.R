68614cbf8d35e10c5d1061c86d593736a88134d7
dcries <dcries@iastate.edu>
2017-06-01 10:48:03 -0500

    update

---
+++ combine_data.R
@@ -0,0 +1,68 @@
+library(ggplot2)
+library(lme4)
+library(dplyr)
+library(reshape)
+library(foreign)
+
+setwd("C:\\Users\\dcries\\github\\epi")
+nhanes <- read.csv("NHANES_accel.csv")
+names(nhanes) <- tolower(names(nhanes))
+
+setwd("\\\\my.files.iastate.edu\\Users\\dcries\\Downloads")
+
+bp1 <- read.xport("BPX_C.xpt")
+bp2 <- read.xport("BPX_D.xpt")
+
+waist1 <- read.xport("BMX_C.xpt")
+waist2 <- read.xport("BMX_D.xpt")
+
+hdl1 <- read.xport("L13_C.xpt")
+hdl2 <- read.xport("HDL_D.xpt")
+
+tri1 <- read.xport("L13AM_C.xpt")
+tri2 <- read.xport("TRIGLY_D.xpt")
+
+glu1 <- read.xport("L10AM_C.xpt")
+glu2 <- read.xport("glu_D.xpt")
+
+
+#blood pressure systotlic and diastolic
+bp <- rbind(bp1[,c("SEQN","BPXSY1","BPXSY2","BPXSY3","BPXSY4","BPXDI1","BPXDI2","BPXDI3","BPXDI4")],
+bp2[,c("SEQN","BPXSY1","BPXSY2","BPXSY3","BPXSY4","BPXDI1","BPXDI2","BPXDI3","BPXDI4")])
+names(bp)[-1] <- c(paste0("bps",1:4),paste0("bpd",1:4))
+
+#waist circumference
+waist <- rbind(waist1[,c("SEQN","BMXWAIST")],
+waist2[,c("SEQN","BMXWAIST")])
+names(waist)[-1] <- "waist"
+
+#tryglyceride mg, LDL mg
+tri <- rbind(tri1[,c("SEQN","LBXTR","LBDLDL")],
+tri2[,c("SEQN","LBXTR","LBDLDL")])
+names(tri)[-1] <- c("tri","ldl")
+
+#hdl mg, 
+names(hdl1)[which(names(hdl1)=="LBXHDD")] <- "LBDHDD"
+hdl <- rbind(hdl1[,c("SEQN","LBDHDD")],
+hdl2[,c("SEQN","LBDHDD")])
+names(hdl)[-1] <- "hdl"
+
+#fasting glucose mg
+glu <- rbind(glu1[,c("SEQN","LBXGLU")],
+glu2[,c("SEQN","LBXGLU")])
+names(glu)[-1] <- "glu"
+
+a=left_join(bp,glu)
+b=left_join(waist,tri)
+d=left_join(a,b)
+full=left_join(d,hdl)
+full$bps <- rowMeans(full[,2:5],na.rm=TRUE)
+full$bpd <- rowMeans(full[,6:9],na.rm=TRUE)
+sum(complete.cases(full[,10:16]))
+
+names(full)[1] <- "id"
+complete=inner_join(nhanes,full[,c(1,10:16)])
+sum(complete.cases(complete))
+length(unique(complete$id[complete.cases(complete)]))
+
+write.csv(complete,file="nhanes_complete.csv",row.names=FALSE)
---
+++ exploratory.R
@@ -0,0 +1,126 @@
+library(ggplot2)
+library(lme4)
+library(dplyr)
+library(reshape)
+
+setwd("C:\\Users\\dcries\\github\\epi")
+nhanes <- read.csv("NHANES_complete.csv")
+names(nhanes) <- tolower(names(nhanes))
+
+nhanes$weekend <- 0
+nhanes$weekend[nhanes$dow %in% c(1,7)] <- 1
+
+sum(nhanes$modvigmetsb==0)/nrow(nhanes)
+sum(nhanes$modvigmin==0)/nrow(nhanes) #not necessarily in a bout, although we have that data
+sum(nhanes$vigmin==0)/nrow(nhanes)
+sum(nhanes$lightmin==0)/nrow(nhanes)
+
+#no diff in modvigmin by day really except 7th
+nhanes %>% group_by(rep) %>% summarise(m=mean(modvigmin),s=sd(modvigmin)/sqrt(length(modvigmin)))
+#no diff in modmin by day really except 7th
+nhanes %>% group_by(rep) %>% summarise(m=mean(modmin),s=sd(modmin)/sqrt(length(modmin)))
+#no diff in modmin by day really except 7th and 2nd
+nhanes %>% group_by(rep) %>% summarise(m=mean(lightmin),s=sd(lightmin)/sqrt(length(lightmin)))
+#mondays most vig, fri-sun least, tues-thurs similar
+nhanes %>% group_by(dow) %>% summarise(m=mean(vigmin),s=sd(vigmin)/sqrt(length(lightmin)))
+#weekends lower
+nhanes %>% group_by(dow) %>% summarise(m=mean(modvigmin),s=sd(modvigmin)/sqrt(length(modvigmin)))
+#weekends lower
+nhanes %>% group_by(dow) %>% summarise(m=mean(modmin),s=sd(modmin)/sqrt(length(modmin)))
+#weekends and thursday lower, friday higher
+nhanes %>% group_by(dow) %>% summarise(m=mean(lightmin),s=sd(lightmin)/sqrt(length(lightmin)))
+#first day vig
+nhanes %>% group_by(rep) %>% summarise(m=mean(vigmin),s=sd(vigmin)/sqrt(length(lightmin)))
+
+#test for weekend effects
+summary(lm(modvigmin ~ as.factor(weekend),data=nhanes))
+summary(lm(modmin ~ as.factor(weekend),data=nhanes))
+summary(lm(lightmin ~ as.factor(weekend),data=nhanes))
+summary(lm(vigmin ~ as.factor(weekend),data=nhanes))
+
+#cor between wear time and exercise time
+cor(nhanes$wearmin,nhanes$lightmin);plot(nhanes$wearmin,nhanes$lightmin)
+cor(nhanes$wearmin,nhanes$modmin);plot(nhanes$wearmin,nhanes$modmin)
+cor(nhanes$wearmin,nhanes$modvigmin);plot(nhanes$wearmin,nhanes$modvigmin)
+cor(nhanes$wearmin,nhanes$vigmin);plot(nhanes$wearmin,nhanes$vigmin)
+
+
+nrep <- (nhanes %>% group_by(id) %>% summarise(n=length(id)))$n
+
+m1 <- lmer(modvigmin~(1|id),data=nhanes)
+
+re <- rep(unlist(ranef(m1)),nrep)
+nhanes$error <- nhanes$modvigmin - re - fixef(m1)
+
+id = unique(nhanes$id)
+acf(nhanes$error[nhanes$id==id[1]])
+
+eht <- nhanes %>% group_by(id) %>% summarise(e1=error[1],e2=error[2],e3=error[3],e4=error[4],e5=error[5],e6=error[6],e7=error[7])
+#eht <- nhanes %>% group_by(id) %>% summarise(e1=modmin[1],e2=modmin[2],e3=modmin[3],e4=modmin[4],e5=modmin[5],e6=modmin[6],e7=modmin[7])
+
+#lag 1 difference
+o1a = c(eht$e1[nrep > 1],eht$e2[nrep > 2],eht$e3[nrep > 3],eht$e4[nrep > 4],eht$e5[nrep > 5],eht$e6[nrep > 6])
+o1b = c(eht$e2[nrep > 1],eht$e3[nrep > 2],eht$e4[nrep > 3],eht$e5[nrep > 4],eht$e6[nrep > 5],eht$e7[nrep > 6])
+cor(o1a,o1b);cor.test(o1a,o1b)
+plot(o1a,o1b)
+#lag 2 diff
+o2a = c(eht$e1[nrep > 2],eht$e2[nrep > 3],eht$e3[nrep > 4],eht$e4[nrep > 5],eht$e5[nrep > 6])
+o2b = c(eht$e3[nrep > 2],eht$e4[nrep > 3],eht$e5[nrep > 4],eht$e6[nrep > 5],eht$e7[nrep > 6])
+cor(o2a,o2b);cor.test(o2a,o2b)
+plot(o2a,o2b)
+#lag 3 diff
+o3a = c(eht$e1[nrep > 3],eht$e2[nrep > 4],eht$e3[nrep > 5],eht$e4[nrep > 6])
+o3b = c(eht$e4[nrep > 3],eht$e5[nrep > 4],eht$e6[nrep > 5],eht$e7[nrep > 6])
+cor(o3a,o3b);cor.test(o3a,o3b)
+plot(o3a,o3b)
+#lag 4 diff
+o4a = c(eht$e1[nrep > 4],eht$e2[nrep > 5],eht$e3[nrep > 6])
+o4b = c(eht$e5[nrep > 4],eht$e6[nrep > 5],eht$e7[nrep > 6])
+cor(o4a,o4b);cor.test(o4a,o4b)
+plot(o4a,o4b)
+#lag 5 diff
+o5a = c(eht$e1[nrep > 5],eht$e2[nrep > 6])
+o5b = c(eht$e6[nrep > 5],eht$e7[nrep > 6])
+cor(o5a,o5b);cor.test(o5a,o5b)
+plot(o5a,o5b)
+
+#test if error larger on day 1 compared to others
+t.test(eht$e1[nrep > 1],eht$e2[nrep > 1])
+all7 <- eht[complete.cases(eht),]
+mall7 <- melt(data.frame(all7[,-1]))
+m2 = lm(value~as.factor(variable),data=mall7)
+summary(m2)
+
+#-------------------------------------------
+#look at relationships among mets risk factors
+y <- nhanes %>% filter(!duplicated(id))
+pairs(y[,c("glu","waist","tri","ldl","hdl","bps","bpd","modvigmin","lightmin","vigmin")])
+
+#correlation plot of variables
+cormat <- cor(y[y$vigmin>0,c("glu","waist","tri","ldl","hdl","bps","bpd","modvigmin","lightmin","vigmin")],use="pairwise.complete.obs")
+cormat[lower.tri(cormat)] <- 0
+diag(cormat) <- 0
+df <- expand.grid(x=1:ncol(cormat),y=1:ncol(cormat))
+df$covar <- c(cormat)
+df$x <- factor(df$x,labels=c("glu","waist","tri","ldl","hdl","bps","bpd","modvigmin","lightmin","vigmin"))
+df$y <- factor(df$y,labels=c("glu","waist","tri","ldl","hdl","bps","bpd","modvigmin","lightmin","vigmin"))
+df$corr <- as.character(c(round(cormat,3)))
+df$corr[df$corr=="0"] <- ""
+
+ggplot(data=df,aes(x=as.factor(x),y=as.factor(y))) + geom_tile(aes(fill=covar)) + 
+  geom_text(aes(label=corr)) +
+  #geom_text(aes(label=count)) + scale_fill_gradient(low = "white", high = "red") 
+  scale_fill_gradient2(high="blue",low="red",mid="white")
+
+#-------------------------
+#multivariate regression on mean observed modvig, light
+a <- nhanes %>% group_by(id) %>% summarise(modvig=mean(modvigmin),light=mean(lightmin))
+y$modvig <- a$modvig
+y$light <- a$light
+
+m3 <- lm(with(y,cbind(glu,waist,tri,hdl,bps,bpd))~y$modvig)
+coef(m3)
+
+m4 <- lm(with(y,cbind(glu,waist,tri,hdl,bps,bpd))~y$light)
+coef(m4)
+
