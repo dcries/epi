b7d17970493bc78cd7f65536ee6b456f009df3c5
unknown <dcries@GS.iastate.edu>
2017-06-29 11:47:04 -0500

    update

--- imputeparallel1.R
+++ imputeparallel1.R
@@ -6,9 +6,9 @@ data <- read.csv("/home/dcries/nhanes_casted.csv")
 nhanes <- read.csv("/home/dcries/epi/nhanes_complete.csv")
 all.equal(unique(data$SEQN),unique(nhanes$id))
 
-flag60 = create.flag(data[1:7*2000,-c(1:3)], window=60)
-label <- data[1:7*2000,1:2]
-demo <- nhanes[!duplicated(nhanes$id),c("sex","age","race","bmi")]
+flag60 = create.flag(data[1:(7*2000),-c(1:3)], window=60)
+label <- data[1:(7*2000),1:2]
+demo <- nhanes[!duplicated(nhanes$id),c("id","sex","age","race","bmi")]
 demo$sex <- as.factor(demo$sex)
 demo$race <- as.factor(demo$race)
 
@@ -22,8 +22,8 @@ registerDoParallel(cores=5)
 getDoParWorkers()
 
 results <- foreach(i=1:5, .export=c('accel.impute'), .packages='accelmissing') %dopar% {  
-  accel.impute(PA=data[1:7*2000,-c(1:3)], label=label, flag=flag60, demo=demo[1:2000,],
-               method="zipln", time.range=c("09:00","20:59"), m=1,seed=seeds[i])
+  accel.impute(PA=data[1:(7*2000),-c(1:3)], label=label, flag=flag60, demo=demo[1:2000,],
+               method="zipln", time.range=c("09:00","20:59"), m=1,seed=seeds[i],demo.include=TRUE)
 }
 
 
--- imputeparallel2.R
+++ imputeparallel2.R
@@ -6,8 +6,8 @@ data <- read.csv("/home/dcries/nhanes_casted.csv")
 nhanes <- read.csv("/home/dcries/epi/nhanes_complete.csv")
 all.equal(unique(data$SEQN),unique(nhanes$id))
 
-flag60 = create.flag(data[(7*2000+1):7*4000,-c(1:3)], window=60)
-label <- data[(7*2000+1):7*4000,1:2]
+flag60 = create.flag(data[(7*2000+1):(7*4000),-c(1:3)], window=60)
+label <- data[(7*2000+1):(7*4000),1:2]
 demo <- nhanes[!duplicated(nhanes$id),c("sex","age","race","bmi")]
 demo$sex <- as.factor(demo$sex)
 demo$race <- as.factor(demo$race)
@@ -22,8 +22,8 @@ registerDoParallel(cores=5)
 getDoParWorkers()
 
 results <- foreach(i=1:5, .export=c('accel.impute'), .packages='accelmissing') %dopar% {  
-  accel.impute(PA=data[(7*2000+1):7*4000,-c(1:3)], label=label, flag=flag60, demo=demo[2001:4000,],
-               method="zipln", time.range=c("09:00","20:59"), m=1,seed=seeds[i])
+  accel.impute(PA=data[(7*2000+1):(7*4000),-c(1:3)], label=label, flag=flag60, demo=demo[2001:4000,],
+               method="zipln", time.range=c("09:00","20:59"), m=1,seed=seeds[i],demo.include=TRUE)
 }
 
 
