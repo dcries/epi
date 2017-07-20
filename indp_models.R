dc75298a1e2cf507aef4be3168e438586eafd733
unknown <dcries@GS.iastate.edu>
2017-07-20 11:23:49 -0500

    update

--- indp_models.R
+++ indp_models.R
@@ -22,6 +22,9 @@ nhanes$active[nhanes$modvigmin ==0] <- 0
 nrep <- (nhanes %>% group_by(id) %>% summarise(n=length(id)))$n
 meas7 <- subset(nhanes, id %in% unique(id)[nrep==7]) #individuals with all 7 days
 
+meas7binom <- meas7 %>% group_by(id) %>% summarise(active=sum(active),sex=sex[1],age=age[1],race=race[1])
+meas7binom$total <- 7
+
 qqnorm(nhanes$modvigmin[nhanes$modvigmin>0]^(1/4));qqline(nhanes$modvigmin[nhanes$modvigmin>0]^(1/4));
 jit <- rnorm(sum(nhanes$modvigmin >0),0,5.5)
 qqnorm((nhanes$modvigmin[nhanes$modvigmin>0]+jit)^(1/4));qqline((nhanes$modvigmin[nhanes$modvigmin>0]+jit)^(1/4));
@@ -44,9 +47,17 @@ m5 <- glm(active~age+as.factor(sex)+as.factor(race)+weekend+as.factor(first5),
                            data=meas7,
                            family=binomial)
 
+m5b <- glm(cbind(active,total-active)~age+as.factor(sex)+as.factor(race),
+          data=meas7binom,
+          family=binomial)
+
 m6 <- glmer(active~age+as.factor(sex)+as.factor(race)+weekend+as.factor(first5)+(1|id),
           data=meas7,
           family=binomial(link="probit"))
+
+m6b <- glmer(cbind(active,total-active)~age+as.factor(sex)+as.factor(race)+(1|id),
+            data=meas7binom,
+            family=binomial(link="probit"))
 
 #--------------------
 #simulate data from model
@@ -55,11 +66,11 @@ final2 <- matrix(0,ncol=10,nrow=nrow(meas7))
 for(j in 1:10){
   simdata <- matrix(0,ncol=length(unique(meas7$id)),nrow=7)
   personeffect <- rnorm(ncol(simdata),0,1.004685)
-  for(i in 1:ncol(simdata)){
-    simdata[,i] <- as.numeric(arima.sim(n=7,list(ar=-0.05532052),sd=0.5601218))+personeffect[i]
-  }
+  # for(i in 1:ncol(simdata)){
+  #   simdata[,i] <- as.numeric(arima.sim(n=7,list(ar=-0.05532052),sd=0.5601218))+personeffect[i]
+  # }
   
-  simdata <- (c(simdata)+m3$fitted[,1])
+  simdata <- (c(simdata)+predict(m6,newdata=meas7,type="response")+personeffect)
   probs <- pnorm(simdata)
   active <- rbinom(length(probs),1,probs)
   final1[j,] <- table(active)
@@ -154,38 +165,38 @@ model
   for (i in 1:N) {
   for(j in 1:K){
     #Y[i,j] <- mu[i,j] + W[i,j]
-    probit(mu[i,j]) <- inprod(X[(i-1)*K+j,],beta[]) + b[i] + W[i,j]
+    probit(mu[i,j]) <- inprod(X[(i-1)*K+j,],beta[]) + b[i]
     Y[i,j] ~ dbern(mu[i,j])
   }
   #Y[i,] ~ dmnorm(mu[i,], Omega[,])
   #mu[i] <- inprod(X[i,],beta[]) + b[i]
   b[i] ~ dnorm(0,tau2b)
-  W[i,1:K] ~ dmnorm(muW[], Omega[,])
+  #W[i,1:K] ~ dmnorm(muW[], Omega[,])
   }
   
-  tau2e ~ dgamma(0.1,0.1)
-  sigma2e <- 1/tau2e
+  #tau2e ~ dgamma(0.1,0.1)
+  #sigma2e <- 1/tau2e
   
   tau2b ~ dgamma(0.1,0.1)
   sigma2b <- 1/tau2b
   
-  rho ~ dunif(-1,1)
+  #rho ~ dunif(-1,1)
   
   beta[1:p] ~ dmnorm(b_mu[], b_var[,])
   
   
-  for (i in 1:(K-1)) {
-  H[i,i] <- sigma2e 
-  for (j in (i+1):K) {
-  H[i,j] <- sigma2e*rho^(abs(i-j))
-  H[j,i] <- H[i,j] 
-  }
-  }
-  
-  H[K,K] <- sigma2e
-  
-  
-  Omega[1:K,1:K] <- inverse(H[,])
+  # for (i in 1:(K-1)) {
+  # H[i,i] <- sigma2e 
+  # for (j in (i+1):K) {
+  # H[i,j] <- sigma2e*rho^(abs(i-j))
+  # H[j,i] <- H[i,j] 
+  # }
+  # }
+  # 
+  # H[K,K] <- sigma2e
+  # 
+  # 
+  # Omega[1:K,1:K] <- inverse(H[,])
 }
 '
 yc2 <- yc[,3:9]
@@ -202,4 +213,65 @@ datajags2 <- list(
 )
 
 jagsModel2 <- jags.model(textConnection(jagsmodel2), data=datajags2,n.adapt=5000,n.chains=3)
-samples2 <- coda.samples(jagsModel2,c("beta","sigma2e","sigma2b","rho"),n.iter=1000)
+samples2 <- coda.samples(jagsModel2,c("beta","sigma2b"),n.iter=5000)
+
+
+
+#-------------
+
+model <- "model{
+  
+  # For the ones trick
+  C <- 10000
+  
+  # for every observation
+  for(i in 1:N){
+      logit(w[i]) <- zeta[i]
+      zeta[i] <- gamma0 + gamma1*age[i] + gamma2*gender[i] + b[ind[i],1]
+    
+      eta[i] <- beta0 + beta1*age[i] + beta2*gender[i] + b[ind[i],2]
+    
+      logGamma[i] <- log(dnorm(y[i], eta[i], tau2))
+    
+    # define the total likelihood, where the likelihood is (1 - w) if y < 0.0001 (z = 0) or
+    # the likelihood is w * gammalik if y >= 0.0001 (z = 1). So if z = 1, then the first bit must be
+    # 0 and the second bit 1. Use 1 - z, which is 0 if y > 0.0001 and 1 if y < 0.0001
+     logLik[i] <- (1 - z[i]) * log(1 - w[i]) + z[i] * ( log(w[i]) + logGamma[i] )
+    
+      Lik[i] <- exp(logLik[i])
+    
+    # Use the ones trick
+      p[i] <- Lik[i] / C
+      ones[i] ~ dbern(p[i])
+
+  }
+  for(i in 1:m){
+    b[i,1:2] ~ dmnorm(mu,Sigma)
+  }
+  beta0 ~ dnorm(0, 0.0001)
+  beta1 ~ dnorm(0, 0.0001)
+  beta2 ~ dnorm(0, 0.0001)
+  gamma0 ~ dnorm(0, 0.0001)
+  gamma1 ~ dnorm(0, 0.0001)
+  gamma2 ~ dnorm(0, 0.0001)
+  tau2 ~ dgamma(1,1)
+  Sigma ~ dwish(D,v)
+}"
+
+dat <- list(
+  N      = nrow(meas7),
+  y      = (meas7$modvigmin)^(1/4),
+  z      = meas7$active,
+  ones = rep(1,nrow(meas7)),
+  age=meas7$age,
+  gender=meas7$sex,
+  D=diag(2),
+  v=3,
+  ind=rep(1:length(unique(meas7$id)),each=7),
+  mu=rep(0,2),
+  m=length(unique(meas7$id))
+)
+
+jm <- jags.model(textConnection(model), data=dat,n.adapt=5000,n.chains=1)
+s <- coda.samples(jm,c("beta0","beta1","beta2","gamma0","gamma1","gamma2","tau2","Sigma"),n.iter=1000)
+
---
+++ stan.R
@@ -0,0 +1,84 @@
+library(rstan)
+
+setwd("C:\\Users\\dcries\\github\\epi")
+setwd("/home/dcries/epi")
+nhanes <- read.csv("NHANES_complete.csv")
+names(nhanes) <- tolower(names(nhanes))
+nhanes$weekend <- 0
+nhanes$weekend[nhanes$dow %in% c(1,7)] <- 1
+nhanes$first5 <- 0
+nhanes$first5[nhanes$rep==6] <- 1
+nhanes$first5[nhanes$rep==7] <- 2
+
+nhanes$active <- 1
+nhanes$active[nhanes$modvigmin ==0] <- 0
+
+nrep <- (nhanes %>% group_by(id) %>% summarise(n=length(id)))$n
+meas7 <- subset(nhanes, id %in% unique(id)[nrep==7]) #individuals with all 7 days
+
+
+models <- "
+data{
+  int<lower=0> N; //number of individuals
+  int<lower=0> k; // number of obs per individual
+  //real<lower=0> y[N];
+  matrix[N,k] y;
+  real<lower=0> age[N];
+  int<lower=0> gender[N];
+}
+parameters{
+//  real gamma0;
+//    real gamma1;
+//real gamma2;
+real beta0;
+real beta1;
+real beta2;
+//cov_matrix[2] Sigma;
+real<lower=0> sigma2e;
+real<lower=-1,upper=1> rho;
+}
+transformed parameters{
+cov_matrix[k] ar1mat;
+
+
+//for (m in 1:k){
+//  Sigma[m,m] <- sigma2e;
+//}
+
+for (m in 1:(k-1)) {
+  for (n in (m+1):k) {
+    ar1mat[m,n] = sigma2e * pow(rho,abs(m-n));
+    ar1mat[n,m] = ar1mat[m,n];
+  }
+}
+
+}
+model{
+
+matrix[N,k] mu;
+
+  for(i in 1:N){
+    for(j in 1:k){
+      mu[i,j] = beta0 + beta1*age[i] + beta2*gender[i];
+    }
+  }
+
+  for(i in 1:N){
+    y[i,] ~ multi_normal(mu[i,],ar1mat);
+  }
+
+  rho ~ uniform(-1,1);
+}
+"
+
+ym <- melt(meas7[,c("modvigmin","rep","id")],id.vars=c("id","rep"))
+yc <- cast(ym,id+variable~rep)
+
+x <- meas7[,c("age","sex","race","weekend","first5")]
+
+dat=list(y=yc[,3:9],  N      = length(unique(meas7$id)),
+         k      = 7, age= meas7$age[!duplicated(meas7$id)],
+         gender= meas7$gender[!duplicated(meas7$id)]
+)
+
+ms <- stan_model(model_code=models)
