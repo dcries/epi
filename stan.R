4a3b1e3a7a29c5db3197a0387cf2b47a2f5a4f5f
unknown <dcries@GS.iastate.edu>
2017-07-21 12:43:05 -0500

    update

--- stan.R
+++ stan.R
@@ -27,6 +27,8 @@ data{
   vector[k] y[N];
   real<lower=0> age[N];
   int<lower=0> gender[N];
+  int<lower=0> numnonzeros[N]; //number of nonzero minutes days for each individual
+  matrix[N,k] nonzeropos; //position of nonzero minutes for each indivudal
   real nu;
   matrix[2,2] D;
 }
@@ -47,25 +49,43 @@ real<lower=-1,upper=1> rho;
 matrix[N,2] b;
 }
 transformed parameters{
-cov_matrix[k] ar1mat;
+cov_matrix[k] ar1mat[N];
 
 
-for (m in 1:k){
-  ar1mat[m,m] = sigma2e;
+//for (m in 1:k){
+//  ar1mat[m,m] = sigma2e;
+//}
+
+//for (m in 1:(k-1)) {
+//  for (n in (m+1):k) {
+//    ar1mat[m,n] = sigma2e * pow(rho,abs(m-n));
+//    ar1mat[n,m] = ar1mat[m,n];
+//  }
+//}
+
+for(i in 1:N){
+  for (m in 1:k){
+    ar1mat[i,m,m] = sigma2e;
+  }
 }
-
-for (m in 1:(k-1)) {
-  for (n in (m+1):k) {
-    ar1mat[m,n] = sigma2e * pow(rho,abs(m-n));
-    ar1mat[n,m] = ar1mat[m,n];
+//respecify ar1mat, nonzeropos, numnonzeros
+for(i in 1:N){
+  for (m in 1:(numnonzeros[i]-1)) {
+    for (n in (m+1):numnonzeros[i]) {
+      ar1mat[i,m,n] = sigma2e * pow(rho,nonzeropos[i,n]-nonzeropos[i,m]);
+      ar1mat[i,n,m] = ar1mat[i,m,n];
+    }
   }
 }
 
 }
 model{
 
-matrix[N,k] mu;
+//matrix[N,k] mu;
+vector[N] mu;
 vector[N] p;
+  int pos;
+  pos = 1;
 
   for(i in 1:N){
     p[i] = Phi(gamma0+gamma1*age[i]+gamma2*gender[i]+b[i,1]);
@@ -81,15 +101,28 @@ vector[N] p;
     }
   }
 
-  for(j in 1:k){
-    for(i in 1:N){
-      mu[i,j] = beta0 + beta1*age[i] + beta2*gender[i] + b[i,2];
-    }
-  }
+//  for(j in 1:k){
+//    for(i in 1:N){
+//      mu[i,j] = beta0 + beta1*age[i] + beta2*gender[i] + b[i,2];
+//    }
+//  }
 
   for(i in 1:N){
-    y[i] ~ multi_normal(row(mu,i),ar1mat);
+    //y[i] ~ multi_normal(row(mu,i),ar1mat);
     b[i] ~ multi_normal(zeros,Sigma);
+  }
+
+//would need to change dimension of mu
+  for(i in 1:N){
+      mu[i] = beta0 + beta1*age[i] + beta2*gender[i] + b[i,2];
+  }
+
+// need to define ind, change y--so it only includes non-zeros
+
+  for(i in 1:N){
+    //segment(y,pos,numnonzeros[i]) ~ multi_normal(rep_vector(mu[i],numnonzeros[i]),block(ar1mat[i],1,1,numnonzeros[i],numnonzeros[i]));
+    segment(y[i],pos,numnonzeros[i]) ~ multi_normal(rep_vector(mu[i],numnonzeros[i]),block(ar1mat[i],1,1,numnonzeros[i],numnonzeros[i]));
+    //pos = pos + numnonzeros[i];
   }
 
   rho ~ uniform(-1,1);
@@ -106,12 +139,27 @@ vector[N] p;
 
 ym <- melt(meas7[,c("modvigmin","rep","id")],id.vars=c("id","rep"))
 yc <- cast(ym,id+variable~rep)
+nonzeros <- (meas7 %>% group_by(id) %>% summarise(n=sum(active)))$n
+a1=meas7$rep[meas7$active==1]
+a2=cumsum(nonzeros)
+
+nonzeropos <- matrix(0,ncol=length(unique(meas7$id)),nrow=7)
+nonzeropos[,1] <- a1[1:nonzeros[1]]
+for(i in 2:ncol(nonzeropos)){
+    temp <- a1[(a2[i-1]+1):a2[i]]
+    if(length(temp)<7){
+      n0 <- 7-length(temp)
+      temp <- c(temp,rep(0,n0))
+    }
+    nonzeropos[,i] <- temp
+}
 
 x <- meas7[,c("age","sex","race","weekend","first5")]
 
 dat=list(y=(yc[,3:9])^(1/4),  N      = length(unique(meas7$id)),
          k      = 7, age= meas7$age[!duplicated(meas7$id)],
-         gender= meas7$sex[!duplicated(meas7$id)],nu=3,D=diag(2)
+         gender= meas7$sex[!duplicated(meas7$id)],nu=3,D=diag(2),
+         numnonzeros=nonzeros,nonzeropos=nonzeropos
 )
 
 rstan_options(auto_write = TRUE)
