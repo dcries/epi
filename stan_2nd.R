e8d53f41032ebabdc2c1bca84b6a9e5cf00c5a7c
unknown <dcries@GS.iastate.edu>
2017-08-21 15:18:34 -0500

    update

---
+++ stan_2nd.R
@@ -0,0 +1,160 @@
+library(rstan)
+library(dplyr)
+setwd("/home/dcries")
+nhanes <- read.csv("epi/nhanes_complete.csv")
+#nhanes <- read.csv("NHANES_complete.csv")
+names(nhanes) <- tolower(names(nhanes))
+
+nhanes$weekend <- 0
+nhanes$weekend[nhanes$dow %in% c(1,7)] <- 1
+nhanes$first5 <- 0
+nhanes$first5[nhanes$rep==6] <- 1
+nhanes$first5[nhanes$rep==7] <- 2
+
+nhanes$active <- 1
+nhanes$active[nhanes$modvigmin ==0] <- 0
+
+nhanes <- subset(nhanes,rep!=7)
+m1 <- lm(modvigmin^.25~(weekend)+first5,data=nhanes)
+wbar <- mean((nhanes$modvigmin[nhanes$rep <= 5])^.25)
+w1 <- nhanes$modvigmin^.25
+what <- predict(m1)
+w <- (1/what)*w1*wbar
+nhanes$modvigmin2 <- w
+
+nrep <- (nhanes %>% group_by(id) %>% summarise(n=length(id)))$n
+meas7 <- subset(nhanes, id %in% unique(id)[nrep==6]) #individuals with all 7 days
+
+
+meas7 <- meas7[(!is.na(meas7$waist)) & (!is.na(meas7$bps)) & (!is.na(meas7$bpd)) & (!is.na(meas7$hdl)) & (!is.na(meas7$ldl)) & (!is.na(meas7$glu)) & (!is.na(meas7$tri)),] #remove NAs for waist
+
+Tstar <- ((meas7 %>% group_by(id) %>% summarise(m=mean(modvigmin)))$m)^.25
+
+models <- "
+data{
+  int<lower=0> N; //number of individuals
+  vector[N] Tstar; // usual ^ 1/4
+
+  real waist[N];
+  real lglu[N];
+  real ltri[N];
+  real bps[N];
+  real ldl[N];
+  real hdl[N];
+  real bpd[N];
+  //real<lower=0> age[N];
+  //int<lower=0> gender[N];
+
+}
+transformed data{
+  //vector[2] zeros;
+  //zeros = rep_vector(0,2);
+}
+parameters{
+  vector[4] alphaw;
+  vector[4] alphag;
+  vector[4] alphat;
+  vector[4] alphabs;
+  vector[3] alphal;
+  vector[3] alphabd;
+  vector[2] alphah;
+
+
+  real<lower=0> sigmawaist;
+  real<lower=0> sigmaglu;
+  real<lower=0> sigmatri;
+  real<lower=0> sigmabps;
+  real<lower=0> sigmabpd;
+  real<lower=0> sigmaldl;
+  real<lower=0> sigmahdl;
+
+}
+
+
+model{
+
+  vector[N] muwaist;
+  vector[N] mulglu;
+  vector[N] multri;
+  vector[N] mubps;
+  vector[N] mubpd;
+  vector[N] muhdl;
+  vector[N] muldl;
+
+
+  muwaist[i] = alphaw[1]-alphaw[2]/(1+exp(-alphaw[3]*(Tstar[i]-alphaw[4])));
+  mulglu[i] = alphag[1]-alphag[2]/(1+exp(-alphag[3]*(Tstar[i]-alphag[4])));
+  multri[i] = alphat[1]-alphat[2]/(1+exp(-alphat[3]*(Tstar[i]-alphat[4])));
+  mubps[i] = alphabs[1]-alphabs[2]/(1+exp(-alphabs[3]*(Tstar[i]-alphabs[4])));
+  muldl[i] = alphal[1] + alphal[2]*Tstar[i] + alphal[3]*pow(Tstar[i],2);
+  mubpd[i] = alphabd[1] + alphabd[2]*Tstar[i] + alphabd[3]*pow(Tstar[i],2);
+  muhdl[i] = alphah[1] + alphah[2]*Tstar[i];
+
+  waist[i] ~ normal(muwaist[i],sigmawaist);
+  lglu[i] ~ normal(mulglu[i],sigmaglu);
+  ltri[i] ~ normal(multri[i],sigmatri);
+  bps[i] ~ normal(mubps[i],sigmabps);
+  ldl[i] ~ normal(muldl[i],sigmaldl);
+  hdl[i] ~ normal(muhdl[i],sigmahdl);
+  bpd[i] ~ normal(mubpd[i],sigmabpd);
+
+
+  sigmawaist ~ cauchy(0,1);
+  sigmaglu ~ cauchy(0,1);
+  sigmatri ~ cauchy(0,1);
+  sigmabps ~ cauchy(0,1);
+  sigmabpd ~ cauchy(0,1);
+  sigmaldl ~ cauchy(0,1);
+  sigmahdl ~ cauchy(0,1);
+
+
+  alphaw ~ normal(0,100);
+  alphag ~ normal(0,100);
+  alphat ~ normal(0,100);
+  alphabs ~ normal(0,100);
+  alphal ~ normal(0,100);
+  alphabd ~ normal(0,100);
+  alphah ~ normal(0,100);
+}
+"
+
+dat=list(N      = length(unique(meas7$id)),
+         #age= meas7$age[!duplicated(meas7$id)],
+         #gender= meas7$sex[!duplicated(meas7$id)],nu=3,D=diag(2),
+         waist=waist,lglu=lglu,ltri=ltri,bps=bps,ldl=ldl,
+         hdl=hdl,bpd=bpd,Tstar=Tstar
+)
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+start1 <- list(alphag=c(0.1642,3.4081,1.4433,4.7228),
+  alphaw=c(10.445,   3.230,   2.033, 101.517 ),
+alphat=c(0.2805, 4.4733, 1.8297, 4.9261 ),
+alphabs=c( 18.388,   4.602 ,  1.389, 137.256 ))
+start2 <- list(alphag=c(0.1242,4.4081,1.1433,3.7228),
+  alphaw=c(8.445,   2.230,   1.033, 80.517 ),
+alphat=c(0.1805, 3.4733, 1.1297, 3.9261 ),
+alphabs=c( 12.388,   3.602 ,  1.189, 120.256 ))
+start3 <- list(alphag=c(0.3642,5.4081,1.9433,5.7228),
+  alphaw=c(11.445,   4.230,   2.933, 111.517 ),
+alphat=c(0.3805, 5.4733, 2.8297,5.9261 ),
+alphabs=c( 19.388,   5.602 ,  2.389, 147.256 ))
+start4 <- list(alphag=c(0.2642,2.4081,2.4433,5.7228),
+               alphaw=c(9.445,   4.230,   1.033, 121.517 ),
+               alphat=c(0.2205, 6.4733, 2.8297, 5.9261 ),
+               alphabs=c( 28.388,   3.602 ,  2.389, 157.256 ))
+
+ms <- stan_model(model_code=models)
+rs <- sampling(ms,dat,c("beta","gamma","sigmae","Sigma","rho","alphaw",
+                        "alphag","alphat","alphal",
+                        "alphabs","alphabd",
+                        "alphah",
+                        "sigmawaist","sigmabps",
+                        "sigmaglu","sigmatri","sigmaldl",
+                        "sigmahdl","sigmabpd"
+),
+iter=2000,chains=4,init=list(start1,start2,start3,start4),
+control=list(adapt_delta=0.8,max_treedepth=12))
+#summary(rs)
+save(rs,file="stanout_2nd.RData")
