c9311f246bdbb6c5a05bf68508525d213f27bed8
Ries <dcries@iastate.edu>
2017-10-06 14:12:39 -0500

    up iterations on real

--- mcmc_realmix3c.R
+++ mcmc_realmix3c.R
@@ -65,7 +65,7 @@ start$currentbeta <- start$currentbeta * .9
 start$currentlambda <- start$currentlambda * .9
 start$Sigmadiag <- start$Sigmadiag * .9
 
-out3 = mcmc_epi_mixture(MetS,tstar2, start, prior, K,300000,100000,thin=10,.2)
+out3 = mcmc_epi_mixture(MetS,tstar2, start, prior, K,450000,150000,thin=10,.2)
 out3$dic
 # pmat <- array(0,dim=c(nrow(out1$beta),nrow(MetS),K))
 # for(i in 1:K){
